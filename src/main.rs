e083907fd04786f29fde5a6563119f3b08ca1e08
ManuelFBS <manuelf.borrego@gmail.com>
2024-01-09 13:02:00 -0400

    Comentando en el main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,26 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 //     format!("Hello {}", &name)
 // }
 
+/// La función `saludos` devuelve una respuesta HTTP con el cuerpo "¡Saludos a todos!" cuando se accede
+/// a la ruta raíz.
 #[get("/")] // La ruta debe ir siempre arriba del método...
 async fn greetings() -> impl Responder {
     HttpResponse::Ok().body("Greetings to everyone !")
 }
 
+/// La función `echo` es una función asincrónica de Rust que toma el cuerpo de una solicitud como
+/// parámetro y devuelve una respuesta HTTP con el mismo cuerpo.
+///
+/// Arguments:
+///
+/// * `req_body`: El parámetro `req_body` es de tipo `String` y representa el cuerpo de la solicitud
+/// HTTP.
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+/// La función `manual_greetings` devuelve una respuesta HTTP con el cuerpo "¡Hola a todos!"
 async fn manual_greetings() -> impl Responder {
     HttpResponse::Ok().body("Hey everybody !")
 }
@@ -31,6 +41,12 @@ async fn manual_greetings() -> impl Responder {
 //     .await
 // }
 
+/// La función anterior configura un servidor web Actix que escucha en el puerto localhost 8085 y maneja
+/// solicitudes de saludos y ecos.
+///
+/// Returns:
+///
+/// La función `main` devuelve un `std::io::Result<()>`.
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
